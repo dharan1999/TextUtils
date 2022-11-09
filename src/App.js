de7f5795939757bdff93c13c74b464d27d5d4fe1
dharan1999 <dharanthakar96@gmail.com>
2022-11-09 15:33:54 -0500

    Completed Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+ import TextForm from './components/TextForm';
+//import About from './components/About';
 import './App.css';
+import React,{ useState } from 'react';
 
 function App() {
+  const [mode,setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = "#3747a1"
+    }else {
+      setMode('light')
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
    <>
-    <Navbar title="textutils2" aboutTextUtils="about1"/>
+    <Navbar title="textutils2" mode = {mode} toggleMode = {toggleMode} aboutTextUtils="about1"/>
     <div className='container my-3'>
-      {/* <TextForm heading="Enter the text to analyze to below"/> */}
-      <About />
+      <TextForm heading="Enter the text to analyze to below" mode = {mode} />
+      {/* <About /> */}
     </div>
       
    </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
-const [text,setText] = useState('Enter text here');
+const [text,setText] = useState('');
 const [vowel,setVowel] = useState(0);
     const handleUpClick = (event) => {
         let newText = text.toUpperCase()
@@ -40,17 +40,17 @@ const [vowel,setVowel] = useState(0);
     }
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === 'dark'?'white' : 'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} style={{backgroundColor: props.mode === 'dark'?'grey' : 'white',color: props.mode === 'dark'?'white' : 'black'}}  onChange={handleChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-2" onClick={handleToggleClick}>Toggle the words</button>
         <button className="btn btn-primary mx-2" value = {vowel} onClick={handleVowelClick}>Count Vowels</button>
     </div>
-    <div className='container my-3'>
+    <div className='container my-3' style={{color: props.mode === 'dark'?'white' : 'black'}}>
         <h1>
             Your text Summary
         </h1>
@@ -59,7 +59,7 @@ const [vowel,setVowel] = useState(0);
         <h2>
             Preview
         </h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter the text in the textbox to preview it"}</p>
         <p>{vowel} number of vowels in text and {text.length - vowel} consonants</p>
 
     </div>
