ce3854eb3b2388271eff91bfdb382af1a883dd9d
dharan1999 <dharanthakar96@gmail.com>
2022-11-16 18:08:58 -0500

    React Router and favicon Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Navbar from './components/Navbar';
  import TextForm from './components/TextForm';
-//import About from './components/About';
+import About from './components/About';
 import './App.css';
 
 import React,{ useState } from 'react';
 import Alert1 from './components/Alert';
-
+import {
+  Routes,Route
+} from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState('light');
+  const [red,setRed] = useState('light');
   const [alert,setAlert] = useState(null);
 
   const showAlert = (message,type) => {
@@ -32,16 +35,30 @@ function App() {
       showAlert("Light Mode has been enabled","success")
     }
   }
+
+  const changeRed = () => {
+    if(red === 'light'){
+      setRed('Red');
+      document.body.style.backgroundColor = '#ff0000'
+      showAlert("Red mode has been enabled","danger")
+    }else {
+      setRed('light')
+      document.body.style.backgroundColor = "white"
+      showAlert("Light Mode has been enabled","success")
+    }
+  }
   return (
    <>
-    <Navbar title="textutils2" mode = {mode} toggleMode = {toggleMode} aboutTextUtils="about1"/>
-      {/* <Alert alert = {alert} /> */}
+    <Navbar title="textutils2" mode = {mode} red = {red} changeRed = {changeRed} toggleMode = {toggleMode} aboutTextUtils="about1"/>
+     
         <Alert1 alert = {alert}></Alert1>
     <div className='container my-3'>
-      <TextForm showAlert = {showAlert} heading="Enter the text to analyze to below" mode = {mode} />
-      {/* <About /> */}
-    </div>
-      
+      <Routes>
+        <Route exact path="/" element={ <TextForm showAlert = {showAlert} heading="Enter the text to analyze to below" mode = {mode} />} />
+        <Route exact path="about" element={<About />}/>
+        
+      </Routes>
+      </div>
    </>
   );
 }
